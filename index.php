07f4a2815446e9c137e539de76a3beffe77cbf7c
Tobang1 <akinolajoseph422@gmail.com>
2022-03-19 21:38:50 +0000

    styling the form page

--- index.php
+++ index.php
@@ -21,7 +21,16 @@ if(!empty($_SESSION['status_response'])){
 
 
     <h1> UPLOAD YOUR CV Testing </h1>
-    <form method="post" action="upload.php" class="form" enctype="multipart/form-data">
+    
+
+    <div class="login-wrap">
+        <div class="login-html">
+            <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Front-End </label>
+            <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Back-End</label>
+            <div class="login-form">
+
+
+             <!-- Front End Developer -->
         
     <div class="sign-in-htm">
                     <div class="group">
@@ -36,21 +45,49 @@ if(!empty($_SESSION['status_response'])){
                         <label for="phone" class="label">Phone Number</label>
                         <input id="phone" type="tel" class="input" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}">
                     </div>
-    </div>
 
 
-
-
-
-
-
-    
-
-        <div class="form-group">
+                    <div class="form-group">
             <label>File</label>
             <input type="file" name="file" class="form-control">
         </div>
         <div class="form-group">
             <input type="submit" class="form-control btn-primary" name="submit" value="Upload"/>
         </div>
+    </div>
+
+
+ <!-- Backend Developer -->
+ <div class="sign-up-htm">
+
+ <div class="group">
+                        <label for="fullname" class="label">Full Name</label>
+                        <input id="fullname" type="text" class="input">
+                    </div>
+                    <div class="group">
+                        <label for="email" class="label">Email</label>
+                        <input id="email" type="email" class="input" >
+                    </div>
+                    <div class="group">
+                        <label for="phone" class="label">Phone Number</label>
+                        <input id="phone" type="tel" class="input" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}">
+                    </div>
+
+
+                    <div class="form-group">
+            <label>File</label>
+            <input type="file" name="file" class="form-control">
+        </div>
+        <div class="form-group">
+            <input type="submit" class="form-control btn-primary" name="submit" value="Upload"/>
+        </div>
+
+
+ </div>
+
+
+
+    
+
+        
     </form>
